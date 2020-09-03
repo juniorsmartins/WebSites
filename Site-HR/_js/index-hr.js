ffc30969aa286e6b813b69017251692f1e77859a
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-03 16:26:21 -0400

    Site HR - v1
    Site HR - v1 : trabalho voluntário para ajudar amigo e ser laboratório de aprendizagem de desenvolvimento Web.

---
+++ Site-HR/_js/index-hr.js
@@ -0,0 +1,15 @@
+<!--
+/*
+  Autor: Junior Martins - https://juniormartins.46graus.com/
+  Objetivo: Trabalho voluntário
+  Cliente: Harrison Ribeiro - HR
+  Descrição: Político - candidato a cargo eletivo
+  Protocolo de Produção: 0020309-2020
+*/
+
+
+
+
+
+
+//-->
