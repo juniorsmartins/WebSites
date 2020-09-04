db304f2a7e4b235957d9341ba8858efbba9298b8
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-04 14:48:08 -0400

    Site HR v1
    Trabalho voluntário

--- Site-HR/Versao001/_js/index-hr.js
+++ Site-HR/Versao001/_js/index-hr.js
@@ -7,8 +7,23 @@
   Protocolo de Produção: 0020309-2020
 */
 
+$(document).ready(function() {
+  $("nav").mouseover(function() {
+    $(".nav-itens").addClass("nav-show");
+  });
+  $("nav").mouseout(function() {
+    $(".nav-itens").removeClass("nav-show");
+  });
 
-
+  $(document).scroll(function(){
+    var topWindow = $(window).scrollTop();
+    if(topWindow > 60) {
+      $("header").addClass('header_fixed');
+    } else {
+        $("header").removeClass('header_fixed')
+    };
+  });
+});
 
 
 
